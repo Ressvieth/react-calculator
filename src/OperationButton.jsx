f86ccfb7d26802f8fa35882cc4e57313ae41d1a5
piotr tylicki <piotr.tylicki@gmail.com>
2025-02-13 20:53:26 +0100

    add calculator

---
+++ src/OperationButton.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+import { ACTIONS } from "./App";
+
+export default function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
+
+OperationButton.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  operation: PropTypes.string.isRequired,
+};
